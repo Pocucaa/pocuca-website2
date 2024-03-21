ea43ab14a8a3fa81d6f0e5f4a7226084584a21fc
Pocucaa <veljkopocuca4@gmail.com>
2024-03-21 19:44:26 +0100

    Co-authored-by: nekagit <nekagit@users.noreply.github.com>

--- src/App.vue
+++ src/App.vue
@@ -2,29 +2,16 @@
 import { RouterLink, RouterView, useRoute } from 'vue-router'
 import "@/assets/css/style.css" 
 
-const route = useRoute()
-
-// Check if the current route is the home route
-const isHomeRoute = route.name === 'Home'
 </script>
 
 <template>
   <div>
-    <nav>
-      <!-- Your navigation elements here -->
-    </nav>
 
-    <!-- Place HomeLayout outside of RouterView -->
-    <HomeLayout v-if="isHomeRoute" />
-
-    <router-view v-if="!isHomeRoute" />
-
-    <CustomFooter />
   </div>
 </template>
 
 
 <style scoped>
-/* Your scoped styles here */
+
 </style>
 
--- src/components/organisms/CustomCard.vue
+++ src/components/organisms/CustomCard.vue
@@ -1,13 +1,24 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
-    <div>
-        Proba
+    <div class="card">
+      <div class="card-content">
+        asdf
+        <slot></slot>
+      </div>
     </div>
-</template>
-
-<style scoped>
-
-</style>
+  </template>
+  
+  <script setup lang="ts">
+  </script>
+  
+  <style scoped>
+  .card {
+    border: 1px solid #ccc; /* Border style */
+    border-radius: 8px; /* Border radius for rounded corners */
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); /* Box shadow for depth */
+  }
+  
+  .card-content {
+    padding: 20px; /* Padding for content */
+  }
+  </style>
+  
